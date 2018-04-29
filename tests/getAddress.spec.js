166d5280d94e997bf405ca097df8219240dc85aa
Jakub Mucha <biuro@muchastudio.com.pl>
2018-04-30 01:17:09 +0200

    added api tests for address-related endpoints

---
+++ tests/getAddress.spec.js
@@ -0,0 +1,45 @@
+const fixtures = require('../fixtures/getAddress');
+
+describe('GET /get_address/{hash_string}', () => {
+  it('should be online', () => {
+    return fixtures
+      .getAddress(process.env.WALLET_HASH)
+      .expect(200)
+      .then(response => {
+        expect(response).toEqual(expect.anything());
+      });
+  });
+});
+
+describe('GET /get_address_abstracts/{hash_string}/{page}', () => {
+  it('should be online', () => {
+    return fixtures
+      .getAddressAbstracts(process.env.WALLET_HASH, 1)
+      .expect(200)
+      .then(response => {
+        expect(response).toEqual(expect.anything());
+      });
+  });
+});
+
+describe('GET /get_address_neon/{hash_string}', () => {
+  it('should be online', () => {
+    return fixtures
+      .getAddressNeon(process.env.WALLET_HASH)
+      .expect(200)
+      .then(response => {
+        expect(response).toEqual(expect.anything());
+      });
+  });
+});
+
+describe('GET /get_address_to_address_abstracts/{hash_string}/{hash_string}/{page}', () => {
+  it('should be online', () => {
+    return fixtures
+      .getAddressToAddressAbstracts(process.env.WALLET_HASH, process.env.WALLET_HASH_SECONDARY, 1)
+      .expect(200)
+      .then(response => {
+        expect(response).toEqual(expect.anything());
+      });
+  });
+});
