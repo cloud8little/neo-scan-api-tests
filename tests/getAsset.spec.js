277fb97bb91eebd9560d8f8ac341a8c80412fe1b
Jakub Mucha <biuro@muchastudio.com.pl>
2018-04-30 01:22:56 +0200

    added api tests for asset-related endpoints

---
+++ tests/getAsset.spec.js
@@ -0,0 +1,26 @@
+const fixtures = require('../fixtures/getAsset');
+
+describe('GET /get_asset/{hash_string}', () => {
+  it('should be online', () => {
+    return fixtures.getAssets().then(response => {
+      const firstAssetTxid = response.body[0].txid;
+      return fixtures
+        .getAsset(firstAssetTxid)
+        .expect(200)
+        .then(response => {
+          expect(response).toEqual(expect.anything());
+        });
+    });
+  });
+});
+
+describe('GET /get_assets', () => {
+  it('should be online', () => {
+    return fixtures
+      .getAssets()
+      .expect(200)
+      .then(response => {
+        expect(response).toEqual(expect.anything());
+      });
+  });
+});
