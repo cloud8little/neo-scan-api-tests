84229b687f887f7e20f138bfcdf7cddf9e680420
Jakub Mucha <biuro@muchastudio.com.pl>
2018-04-30 01:17:44 +0200

    added api tests for block-related endpoints

---
+++ tests/getBlock.spec.js
@@ -0,0 +1,29 @@
+const fixtures = require('../fixtures/getBlock');
+
+describe('GET /get_block/{hash_string}', () => {
+  it('should be online', () => {
+    return fixtures.getHighestBlock().then(response => {
+      const blockHash = response.body.hash;
+      return fixtures
+        .getBlock(blockHash)
+        .expect(200)
+        .then(response => {
+          expect(response).toEqual(expect.anything());
+        });
+    });
+  });
+});
+
+describe('GET /get_block/{height}', () => {
+  it('should be online', () => {
+    return fixtures.getHeight().then(response => {
+      const blockHeight = response.body.height;
+      return fixtures
+        .getBlock(blockHeight)
+        .expect(200)
+        .then(response => {
+          expect(response).toEqual(expect.anything());
+        });
+    });
+  });
+});
