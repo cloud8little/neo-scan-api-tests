2954d679a148eccff86b36cec6280c81b19adbb4
Jakub Mucha <biuro@muchastudio.com.pl>
2018-05-01 00:04:38 +0200

    added more tests for block-related endpoints

--- tests/getBlock.spec.js
+++ tests/getBlock.spec.js
@@ -1,5 +1,38 @@
 const fixtures = require('../fixtures/getBlock');
 
+describe('GET /get_height', () => {
+  it('should be online', () => {
+    return fixtures
+      .getHeight()
+      .expect(200)
+      .then(response => {
+        expect(response).toEqual(expect.anything());
+      });
+  });
+});
+
+describe('GET /get_highest_block', () => {
+  it('should be online', () => {
+    return fixtures
+      .getHighestBlock()
+      .expect(200)
+      .then(response => {
+        expect(response).toEqual(expect.anything());
+      });
+  });
+});
+
+describe('GET /get_last_blocks', () => {
+  it('should be online', () => {
+    return fixtures
+      .getLastBlocks()
+      .expect(200)
+      .then(response => {
+        expect(response).toEqual(expect.anything());
+      });
+  });
+});
+
 describe('GET /get_block/{hash_string}', () => {
   it('should be online', () => {
     return fixtures.getHighestBlock().then(response => {
