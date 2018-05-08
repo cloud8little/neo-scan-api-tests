a0bbad717a90a06cc039a3850b11deeccbb70f35
Jakub Mucha <biuro@muchastudio.com.pl>
2018-05-08 22:06:57 +0200

    added ftp config

--- helpers/config.js
+++ helpers/config.js
@@ -1,4 +1,9 @@
 const config = {
+  ftp: {
+    host: '<PUT_HOST_HERE>',
+    user: '<PUT_USERNAME_HERE>',
+    pass: '<PUT_PASSWORD_HERE>',
+  },
   getHost: () => {
     if (process.env.CI_NETWORK) {
       if (process.env.CI_NETWORK.toLowerCase().includes('test')) return process.env.TESTNET_URL;
