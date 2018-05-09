3ae4cada69d6470711e8e416c71a923e9cb2a0c7
Jakub Mucha <biuro@muchastudio.com.pl>
2018-05-10 00:55:28 +0200

    moved part of config to settings

--- helpers/config.js
+++ helpers/config.js
@@ -1,9 +1,4 @@
 const config = {
-  gitlab: {
-    hostUrl: '<PUT_GITLAB_HOST_URL_HERE>', // example: 'https://gitlab.com' - not required if `GITLAB_HOST_URL` env var is set instead
-    personalToken: '<PUT_GITLAB_PERSONAL_TOKEN_HERE>', // not required if `GITLAB_PERS_TOKEN` env var is set instead
-    statusProjectId: 2, // not required if `GITLAB_STATUS_PROJ_ID` env var is set instead
-  },
   getHost: () => {
     if (process.env.CI_NETWORK) {
       if (process.env.CI_NETWORK.toLowerCase().includes('test')) return process.env.TESTNET_URL;
