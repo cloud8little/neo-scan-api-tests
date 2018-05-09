51e99680c7c9217b81bd5f7a822bcdca7e226bf7
Jakub Mucha <biuro@muchastudio.com.pl>
2018-05-10 00:57:48 +0200

    preparing refactor of fixtures

--- helpers/config.js
+++ helpers/config.js
@@ -1,10 +1,24 @@
+const supertest = require('supertest');
+const request = supertest(getHost());
+
+function getHost() {
+  if (process.env.CI_NETWORK) {
+    if (process.env.CI_NETWORK.toLowerCase().includes('test')) return process.env.TESTNET_URL;
+    else return process.env.MAINNET_URL;
+  } else if (process.env.NETWORK.toLowerCase().includes('test')) return process.env.TESTNET_URL;
+  return process.env.MAINNET_URL;
+}
+
 const config = {
-  getHost: () => {
-    if (process.env.CI_NETWORK) {
-      if (process.env.CI_NETWORK.toLowerCase().includes('test')) return process.env.TESTNET_URL;
-      else return process.env.MAINNET_URL;
-    } else if (process.env.NETWORK.toLowerCase().includes('test')) return process.env.TESTNET_URL;
-    return process.env.MAINNET_URL;
+  req: (url, type = 'get') => {
+    console.info(`[${type.toUpperCase()}] ${getHost()}${url}`);
+    if (type === 'get') {
+      return request.get(url);
+    } else if (type === 'post') {
+      return request.post(url);
+    } else if (type === 'put') {
+      return request.put(url);
+    }
   },
 };
 
