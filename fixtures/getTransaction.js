ae4a8f0e309754bf9be3ff222f81bccdb5fbd101
Jakub Mucha <biuro@muchastudio.com.pl>
2018-07-09 01:13:51 +0200

    removed tests for removed endpoints related to getTransaction

--- fixtures/getTransaction.js
+++ fixtures/getTransaction.js
@@ -4,13 +4,6 @@ const getTransaction = {
   getTransaction: hash => {
     return req(`/get_transaction/${hash}`);
   },
-  getLastTransactions: transType => {
-    if (transType) {
-      return req(`/get_last_transactions/${transType}`);
-    } else {
-      return req(`/get_last_transactions`);
-    }
-  },
   getLastTransactionsByAddress: (hash, page) => {
     return req(`/get_last_transactions_by_address/${hash}/${page}`);
   },
