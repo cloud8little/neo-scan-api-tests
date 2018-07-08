f2a4b90bc17210764bd609a23b6af9c3ae05de36
Jakub Mucha <biuro@muchastudio.com.pl>
2018-07-09 01:15:16 +0200

    updated test for get_block endpoint

--- tests/getBlock.spec.js
+++ tests/getBlock.spec.js
@@ -13,14 +13,17 @@ describe('GET /get_height', () => {
 
 describe('GET /get_block/{hash_string}', () => {
   it('should be online', () => {
-    return fixtures.getHighestBlock().then(response => {
-      const blockHash = response.body.hash;
-      return fixtures
-        .getBlock(blockHash)
-        .expect(200)
-        .then(response => {
-          expect(response.body).toEqual(expect.anything());
-        });
+    return fixtures.getHeight().then(response => {
+      const blockHeight = response.body.height;
+      return fixtures.getBlock(blockHeight).then(response => {
+        const blockHash = response.body.hash;
+        return fixtures
+          .getBlock(blockHash)
+          .expect(200)
+          .then(response => {
+            expect(response.body).toEqual(expect.anything());
+          });
+      });
     });
   });
 });
