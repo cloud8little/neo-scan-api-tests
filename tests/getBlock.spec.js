4007114cfbaf066917ca8c9c2d8d6512ae425988
Jakub Mucha <biuro@muchastudio.com.pl>
2018-07-09 01:15:02 +0200

    removed tests for removed endpoints related to getBlock

--- tests/getBlock.spec.js
+++ tests/getBlock.spec.js
@@ -11,28 +11,6 @@ describe('GET /get_height', () => {
   });
 });
 
-describe('GET /get_highest_block', () => {
-  it('should be online', () => {
-    return fixtures
-      .getHighestBlock()
-      .expect(200)
-      .then(response => {
-        expect(response.body).toEqual(expect.anything());
-      });
-  });
-});
-
-describe('GET /get_last_blocks', () => {
-  it('should be online', () => {
-    return fixtures
-      .getLastBlocks()
-      .expect(200)
-      .then(response => {
-        expect(response.body).toEqual(expect.anything());
-      });
-  });
-});
-
 describe('GET /get_block/{hash_string}', () => {
   it('should be online', () => {
     return fixtures.getHighestBlock().then(response => {
